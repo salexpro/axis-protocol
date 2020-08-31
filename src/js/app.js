072892b7bdef67c8bc0e6ea94582a5f54f52089f
Alexander <dev@salex.pro>
2020-08-31 04:03:27 +0300

    demo update

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,12 @@
+/* global $ */
+import 'zepto/dist/zepto.min';
 // import svg4everybody from 'svg4everybody';
 // import './lib/foundation-explicit-pieces';
 
 // svg4everybody();
 
-/* global $ */
-import 'zepto/dist/zepto.min';
-
-const toggleHeader = () => {
+const toggleHeader = () =>
   $('.header').toggleClass('shrink', $(window).scrollTop() > 100);
-};
-$(document).on('scroll', toggleHeader);
 
+$(document).on('scroll', toggleHeader);
 toggleHeader();
