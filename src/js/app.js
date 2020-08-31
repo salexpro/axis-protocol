a2c183f4dba9506c2d10a7edec6a7fb0232c813b
Alexander <dev@salex.pro>
2020-08-31 03:59:42 +0300

    landing page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,14 @@
-import svg4everybody from 'svg4everybody';
+// import svg4everybody from 'svg4everybody';
 // import './lib/foundation-explicit-pieces';
 
-svg4everybody();
+// svg4everybody();
+
+/* global $ */
+import 'zepto/dist/zepto.min';
+
+const toggleHeader = () => {
+  $('.header').toggleClass('shrink', $(window).scrollTop() > 100);
+};
+$(document).on('scroll', toggleHeader);
+
+toggleHeader();
